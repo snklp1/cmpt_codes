e08b70eedc7338f8ce13fc0bcd09a674772bc731
Sankalp Mahajan <77531262+snklp1@users.noreply.github.com>
2021-03-12 13:42:35 +0530

    question 31
    https://www.hackerearth.com/problem/algorithm/city-tour/

---
+++ q31.cpp
@@ -0,0 +1,21 @@
+//the very easy question if you get it type.
+
+
+//https://www.hackerearth.com/problem/algorithm/city-tour/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int testcases;
+    cin>>testcases;
+    while(testcases--)
+    {
+        long long int n;
+        long long int answer;
+        cin>>n;
+        answer = n*4;
+        cout<<answer<<endl;
+    }
+}
