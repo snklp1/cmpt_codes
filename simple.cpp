7ce562066eedf56405264ed7470a9734f178df94
Sankalp Mahajan <77531262+snklp1@users.noreply.github.com>
2021-05-23 00:21:41 +0530

    never get intimidated
    the question mentioned things I didnt know. Never be intimidated.
    https://codeforces.com/problemset/problem/266/B

---
+++ simple.cpp
@@ -0,0 +1,32 @@
+//https://codeforces.com/problemset/problem/266/B
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    int n,t;
+    cin>>n>>t;
+
+    string s;
+    cin>>s;
+
+    while(t--)
+    {
+        for(int i=0;i<s.length()-1;i++)
+        {
+            if(s[i]=='B'&&s[i+1]=='G')
+            {
+                s[i] = 'G';
+                s[i+1] = 'B';
+                i++;
+            }
+        }
+    }
+
+    cout<<s<<endl;
+
+    
+    return 0;
+}
