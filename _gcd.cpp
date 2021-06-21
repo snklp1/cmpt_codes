2f99987bbd70fb9af0cd357bc93ea05ce3e55b14
Sankalp Mahajan <77531262+snklp1@users.noreply.github.com>
2021-06-21 22:52:14 +0530

    Good question with O(1) complexity
    https://codeforces.com/problemset/problem/1370/A

---
+++ _gcd.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Author: snklp1
+
+#define llint long long int
+#define lint long int
+
+//int fun()
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        if(n>=2)
+        {
+            cout<<n/2<<endl;
+        }
+        else
+        {
+            cout<<1<<endl;
+        }
+    }
+
+    return 0;
+}
+
+
+//end
