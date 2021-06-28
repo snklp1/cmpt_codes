be8ef0db53ab200fac80c19ee884f8b5efea90ae
Sankalp Mahajan <77531262+snklp1@users.noreply.github.com>
2021-06-28 23:38:05 +0530

    easiest code ever
    too fucking easy.
    https://codeforces.com/problemset/problem/630/A

---
+++ easiest.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Author: snklp1
+
+#define llint long long int
+#define lint long int
+
+//int fun()
+
+
+int main()
+{
+	string s;
+	cin>>s;
+	cout<<"25"<<endl;
+}
+
+
+//end
