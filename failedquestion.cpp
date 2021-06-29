6870604d2ccb75a8734ee832dc6a05454231a472
Sankalp Mahajan <77531262+snklp1@users.noreply.github.com>
2021-06-29 23:50:54 +0530

    omkar dude
    the questioner failed.
    https://codeforces.com/problemset/problem/1372/A

---
+++ failedquestion.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Author: snklp1
+
+#define llint long long int
+#define lint long int
+
+//int fun()
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		for (int i = 0; i < n; ++i)
+		 {
+		 	/* code */
+		 	cout<<"1 ";
+		 } 
+		 cout<<endl;
+	}
+
+    return 0;
+}
+
+
+//end
