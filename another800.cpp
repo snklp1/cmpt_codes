2368633f4d6b66c4c59f0e067977f83fd8d370f5
Sankalp Mahajan <77531262+snklp1@users.noreply.github.com>
2021-07-01 22:03:19 +0530

    stop doing 800
    https://codeforces.com/problemset/problem/959/A

---
+++ another800.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Author: snklp1
+
+#define llint long long int
+#define lint long int
+
+//int fun()
+
+
+int main()
+{
+	lint n;
+	cin>>n;
+
+	if(n%2==0)
+	{
+		cout<<"Mahmoud"<<endl;
+	}
+	else
+	{
+		cout<<"Ehab"<<endl;
+	}
+
+    return 0;
+}
+
+
+//end
