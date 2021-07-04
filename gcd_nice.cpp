84a00a13f05fd95c72bd0339cfe1df3251968f06
Sankalp Mahajan <77531262+snklp1@users.noreply.github.com>
2021-07-04 23:42:42 +0530

    gcd + lcm
    https://codeforces.com/problemset/problem/1325/A

---
+++ gcd_nice.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Author: snklp1
+
+#define llint long long int
+#define lint long int
+
+//int fun()
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		lint n;
+		cin>>n; 
+		cout<<1<<" "<<n-1<<endl;
+	}
+
+    return 0;
+}
+
+
+//end
