397f1d927811b8214d4b5f703b5436b2761e460e
Sankalp Mahajan <77531262+snklp1@users.noreply.github.com>
2021-07-05 23:26:33 +0530

    accepted
    fastest you've ever done a 900

---
+++ queued.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Author: snklp1
+
+#define llint long long int
+#define lint long int
+
+//int fun()
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		lint n;
+		cin>>n; 
+
+		
+		int moves = -1;
+
+		while(n>=1)
+		{
+			moves++;
+			if(n==1)
+			{
+				break;
+			}
+			else
+			{
+				if(n%6==0)
+				{
+					n=n/6;
+				}
+				else
+				{
+					if(n%3==0)
+					{
+						n=n*2;
+					}
+					else
+					{
+						//cout<<-1<<endl;
+						moves  = -1;
+						break;
+					}
+				}
+			}
+		}
+
+		cout<<moves<<endl;
+	}
+
+    return 0;
+}
+
+
+//end
