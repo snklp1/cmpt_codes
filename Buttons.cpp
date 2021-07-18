8cf3bd41cafb7a3f3ef4488186f41105e3e5d277
Sankalp Mahajan <77531262+snklp1@users.noreply.github.com>
2021-07-18 23:29:56 +0530

    still_queued.cpp
    https://codeforces.com/problemset/problem/268/B

---
+++ Buttons.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+//Author: snklp1
+ 
+#define llint long long int
+#define lint long int
+ 
+//int fun()
+ 
+//7
+ 
+ 
+int main()
+{
+	int n;
+	cin>>n;
+	int temp = n;
+	llint ans = 0;
+ 
+	ans = (n-1)*(n-1) + n;
+	
+ 
+ 
+	cout<<ans<<endl;
+    return 0;
+}
+ 
+ 
+//end
