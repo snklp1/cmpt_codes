ca15ec63a8b5b44fc236276358ec66a8cb48c919
Sankalp Mahajan <77531262+snklp1@users.noreply.github.com>
2021-08-02 23:36:25 +0530

    very easy
    https://codeforces.com/problemset/problem/1391/A

---
+++ little_checks.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Author: snklp1
+//TE: Sublime Text 3
+//snpt(1) ok_start
+
+#define llint     	long long int
+#define lint      	long int
+#define vint      	vector<int>
+#define vllint    	vector<long long int> 
+#define vpint     	vector<pair<int,int>> 
+#define vpllint   	vector<pair<long long int, long long int>> 
+#define MP        	make_pair
+#define PB        	push_back
+#define POB       	pop_back
+#define all(v)    	(v).begin(),(v).end()
+
+//int fun()
+
+void yes(){cout<<"YES"<<endl;}
+void no(){cout<<"NO"<<endl;}
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n; 
+		for (int i = 0; i < n; ++i)
+		{
+			/* code */
+			cout<<i+1<<" ";
+		}
+		cout<<endl;
+	}
+
+    return 0;
+}
+
+
+//end
