f1d84793bb87982758e4a2e69902953e43252fce
Sankalp Mahajan <77531262+snklp1@users.noreply.github.com>
2021-08-05 23:38:36 +0530

    10^100 input
    https://codeforces.com/problemset/problem/1455/A

---
+++ grandlyeasy.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Author: snklp1
+//TE: Sublime Text 3
+//snpt(1) ok_start
+
+#define llint     	long long int
+#define lint      	long int
+#define vint      	vector<int>
+#define vllint    	vector<long long int> 
+#define vpint     	vector<pair<int,int>> 
+#define vpllint   	vector<pair<long long int, long long int>> 
+#define MP        	make_pair
+#define PB        	push_back
+#define POB       	pop_back
+#define all(v)    	(v).begin(),(v).end()
+
+//int fun()
+
+void yes(){cout<<"YES"<<endl;}
+void no(){cout<<"NO"<<endl;}
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		cin>>s;
+		cout<<s.length()<<endl;
+	}
+
+    return 0;
+}
+
+
+//end
