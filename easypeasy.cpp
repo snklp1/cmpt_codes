ef1d803821fc5622d56dcce24c1728b790758ba2
Sankalp Mahajan <77531262+snklp1@users.noreply.github.com>
2021-08-16 23:23:41 +0530

    just killed mosquito easily
    https://codeforces.com/problemset/problem/1324/A

---
+++ easypeasy.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Author: snklp1
+//TE: Sublime Text 3
+//snpt(1) ok_start
+
+#define llint     	long long int
+#define lint      	long int
+#define vint      	vector<int>
+#define vllint    	vector<long long int> 
+#define vpint     	vector<pair<int,int>> 
+#define vpllint   	vector<pair<long long int, long long int>> 
+#define MP        	make_pair
+#define PB        	push_back
+#define POB       	pop_back
+#define all(v)    	(v).begin(),(v).end()
+
+//int fun()
+
+void yes(){cout<<"YES"<<endl;}
+void no(){cout<<"NO"<<endl;}
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n; 
+		int even = 0,odd=0;
+		int tetris[n];
+		for (int i = 0; i < n; ++i)
+		{
+			/* code */
+			cin>>tetris[i];
+			if(tetris[i]%2==0)
+			{
+				even++;
+			}
+			else
+			{
+				odd++;
+			}
+		}
+
+		if(even==0||odd==0)
+		{
+			yes();
+		}
+		else
+		{
+			no();
+		}
+	}
+
+    return 0;
+}
+
+
+//end
